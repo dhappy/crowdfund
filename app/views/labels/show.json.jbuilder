e1283354adf353ca8de5c91318f3f5314ee9db28
Will Holcomb <will@dhappy.org>
2014-03-03 12:29:00 -0500

    rails g scaffold label name:string color:string

---
+++ app/views/labels/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @label, :id, :name, :color, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 App::Application.routes.draw do
+  resources :labels
+
   resources :milestones
 
   resources :issues
