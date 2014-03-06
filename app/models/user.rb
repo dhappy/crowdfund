a8a309fe922232d030013e369392adb64dca6b94
Will Holcomb <will@dhappy.org>
2014-03-06 16:09:28 -0500

    allowing empty email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :validatable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
+
+  validates_uniqueness_of :email, case_sensitive: false, allow_blank: true, if: :email_changed?
+  validates_format_of :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
+  validates_confirmation_of :password, on: :create
+  validates_length_of :password, within: Devise.password_length, allow_blank: true
 
   has_many :projects, class_name: 'Project', foreign_key: 'owner_id'
   has_many :organizations
