9ed50b9ca03715f46d1a4c51c7aed0559bc28fe8
Will Holcomb <will@dhappy.org>
2014-03-10 13:51:49 -0400

    allowing adding projects

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -41,7 +41,7 @@ class OrganizationsController < ApplicationController
   # PATCH/PUT /organizations/1.json
   def update
     respond_to do |format|
-      if @organization.update(organization_params)
+      if @organization.update organization_params
         format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
         format.json { head :no_content }
       else
@@ -69,6 +69,6 @@ class OrganizationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def organization_params
-      params.require(:organization).permit(:name)
+      params.require(:organization).permit(:name, project_ids: [])
     end
 end
