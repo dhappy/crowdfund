9d84a643159eaf2ca1c33526a6abf5e20c8ecf72
Will Holcomb <will@dhappy.org>
2014-03-10 21:46:01 -0400

    adding projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 org = Organization.create name: 'Department of Happiness'
-projs = [ { owner: 'OneAcreCafe', github_name: 'volunteers' } ]
+projs = [ { owner: 'dhappy', github_name: 'crowdfund' },
+          { owner: 'dhappy', github_name: 'tour' },
+          { owner: 'OneAcreCafe', github_name: 'volunteers' },
+          { owner: 'dhappy', github_name: 'housing' },
+          { owner: 'OneAcreCafe', github_name: 'sales' },
+          { owner: 'dhappy', github_name: 'liquid_vote' },
+          { owner: 'OneAcreCafe', github_name: 'costs' },
+          { owner: 'wholcomb', github_name: 'mimis' } ]
 projs.each do |proj|
   project = Project.load_from_github proj
   org.projects << project
