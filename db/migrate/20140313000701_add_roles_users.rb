79663b339aa4e4e60493840fc570b9f666d5f141
Will Holcomb <will@dhappy.org>
2014-03-12 20:07:59 -0400

    failed rails g migration add_roles_users user:references role:references

---
+++ db/migrate/20140313000701_add_roles_users.rb
@@ -0,0 +1,8 @@
+class AddRolesUsers < ActiveRecord::Migration
+  def change
+    create_table :roles_users do |t|
+      t.references :user, index: true
+      t.references :role, index: true
+    end
+  end
+end
