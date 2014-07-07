c3c9cbb5592ee3d6ee509675282aeaf9aa706471
Will Holcomb <will@dhappy.org>
2014-07-07 12:46:24 -0400

    fixing type

--- db/migrate/20140707063842_add_min_max_to_bid.rb
+++ db/migrate/20140707063842_add_min_max_to_bid.rb
@@ -1,6 +1,6 @@
 class AddMinMaxToBid < ActiveRecord::Migration
   def change
-    add_column :bids, :min, :number
-    add_column :bids, :max, :number
+    add_column :bids, :min, :decimal
+    add_column :bids, :max, :decimal
   end
 end
