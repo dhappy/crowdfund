3f53b8ed7a505c0d4f47fa6ae5ec08a4b53ba512
Will Holcomb <will@dhappy.org>
2014-07-07 15:02:09 -0400

    allowing editing user w/o password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   def update
+    @user = current_user
     account_update_params = devise_parameter_sanitizer.sanitize :account_update
 
     if account_update_params[:password].blank?
@@ -7,10 +8,17 @@ class RegistrationsController < Devise::RegistrationsController
       account_update_params.delete 'password_confirmation'
     end
 
-    @user = current_user
-    if update_resource @user, account_update_params
+    successfully_updated =
+      if needs_password?(@user, params)
+        @user.update_with_password account_update_params
+      else
+        params[:user].delete(:current_password)
+        @user.update_without_password account_update_params
+      end
+
+    if successfully_updated
       set_flash_message :notice, :updated
-      sign_in @user, bypass: true
+      sign_in @user, :bypass => true
       redirect_to after_update_path_for @user
     else
       render 'edit'
@@ -21,5 +29,11 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for resource
     root_path
+  end
+
+  private
+  
+  def needs_password?(user, params)
+    user.email != params[:user][:email] or params[:user][:password].present?
   end
 end
